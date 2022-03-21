62206020abc524057d13228c62b8d6a44041e1f4
pibiCo <pibico.sl@gmail.com>
2022-03-21 13:56:28 +0100

    modified:   pibidesk/jinja_filters.py

--- pibidesk/jinja_filters.py
+++ pibidesk/jinja_filters.py
@@ -7,7 +7,10 @@ import base64
 from io import BytesIO
 from datetime import tzinfo, timedelta, datetime
 
-def timestamp_to_date(value, format='%a %d/%m/%y %H:%M'):
+def timestamp_to_date(value, format='%H:%M'):
+  if value:
+    return datetime.fromtimestamp(int(value)).strftime(format)
+def ts_to_date(value, format='a% %d/%m/%y %H:%M'):
   if value:
     return datetime.fromtimestamp(int(value)).strftime(format)
 
